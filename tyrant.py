63c5d406ef9de99ae2ddd48528a2ef8d0c8dee13
someonewhocanbarelycode1 <109048795+someonewhocanbarelycode1@users.noreply.github.com>
2022-07-10 23:25:30 -0400

    Delete tyrant.py

--- tyrant.py
+++
@@ -1,3 +0,0 @@
-def call():
-    import keyboard
-    keyboard.write("When you need someone to fix your sink, you call a plumber. When you need someone to fix your electricity, you call an electrician. And when you need someone to OVERTHROW A BRUTAL TYRANT, YOU CALL AN ANARCHIST!")
