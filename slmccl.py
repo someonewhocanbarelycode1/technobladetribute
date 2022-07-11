f5d83b04f158b2351239e3641eb28d6d72db596c
someonewhocanbarelycode1 <109048795+someonewhocanbarelycode1@users.noreply.github.com>
2022-07-10 23:53:56 -0400

    Delete slmccl.py

--- slmccl.py
+++
@@ -1,3 +0,0 @@
-def anotherwrite():
-    import keyboard
-    keyboard.write("It kinda reminds me of me, in that you can hear the videogamedunkey bleeding through. Which is always a good thing. I stan videogamedunkey okay. Just a huge clone of him.")
