d2668333c9271d5e49d993e5b9c29ff19706501a
someonewhocanbarelycode1 <109048795+someonewhocanbarelycode1@users.noreply.github.com>
2022-07-10 23:50:13 -0400

    Delete duel.py

--- duel.py
+++
@@ -1,3 +0,0 @@
-def ad():
-    import keyboard
-    keyboard.write("I tried to raise my shield, but I accidentally sheared the ground, or whatever you call this mechanic* . All I know is that I clearly did not take my ADHD medicine this morning, because I immediately become distracted and forget about the duel for about ten seconds, but then I remember \"Oh, right, $100,000\" and go in for the kill.")
