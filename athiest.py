ab545d713e052f76e8211b822031293e0c79eb51
someonewhocanbarelycode1 <109048795+someonewhocanbarelycode1@users.noreply.github.com>
2022-07-10 22:43:07 -0400

    Add files via upload

---
+++ athiest.py
@@ -0,0 +1,5 @@
+def kkg():
+    import keyboard
+    keyboard.write("Look, I\'m an atheist, but when God sends me to Hell I want him to hesitate.")
+
+# keyboard.write("")
---
+++ hm.py
@@ -0,0 +1,5 @@
+import vc
+import sys
+stat='idlelib' in sys.modules
+if stat == False:
+    input('Press ENTER to exit')
