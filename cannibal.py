87d86b4e40d3e5e01547d52bab063e27712a593d
someonewhocanbarelycode1 <109048795+someonewhocanbarelycode1@users.noreply.github.com>
2022-07-11 22:49:51 -0400

    Delete cannibal.py

--- cannibal.py
+++
@@ -1,3 +0,0 @@
-def writeitloser():
-    import keyboard
-    keyboard.write("Now, I know what you\'re gonna say:\"Technoblade, you monster! You really killed millions of your own kind just to make a sword?\". And it- look, look, look, come on, you gotta understand. There was nothing else I could do, I was mildly inconvenienced!")
